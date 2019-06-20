2958eee954401d8c1f86d3c92426b581b5c679f5
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-06-20 16:27:10 +0500

    Compute the value

---
+++ value.py
@@ -0,0 +1,9 @@
+#Question num 4:
+
+'''   Compute the value of (n + nn + nnn)   '''
+
+
+n = int(input("Enter an integer number: "))
+
+value = n + (n*n) + (n*n*n)
+print("Value of (n+nn+nnn) =",value) 
