d837a0df92f9798073ca1ce204a4c4d088aee67a
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-06-20 16:15:30 +0500

    Number is completely divisible by another number

---
+++ check_div.py
@@ -0,0 +1,15 @@
+#Question num 3:
+
+'''   Check number is completely divisible by another number   '''
+
+num = int(input('enter the num'))
+
+if num == 2:
+    print('num is divide by 2')
+elif num == 5:
+    print('num is not divisible by another num')
+elif num == 8:
+    print('num is divided by 2')
+else:
+    num == 7 , 9 , 11
+    print('num is not divisible by another num')
