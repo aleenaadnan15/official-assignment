f6ea37bbc7cfed8d8a81cde37e267ba929cd26ff
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-06-20 16:13:33 +0500

    Check num is positive , neg or zero

---
+++ check_num.py
@@ -0,0 +1,16 @@
+#Question num 2:
+
+'''   Check the num is + , - or 0   '''
+
+num = int(input('enter the num'))
+
+if num < 0:
+    print('num is negative')
+elif num == 0:
+    print('num is positive')
+elif num == 1:
+    print('num is positive')
+else:
+    num > 2
+    result = 0
+    print('num is zero')
