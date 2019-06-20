9ca3d618707015350d7bf4ed9d2cdcb46390639e
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-06-20 16:11:50 +0500

    Area of Cirlce

---
+++ circle.py
@@ -0,0 +1,8 @@
+#Question num 1:
+
+'''    Area of Cirlce   '''
+
+import math
+radius = int(input('enter radius of cirlce'))
+area = round(math.pi * radius**2,2)
+print('area of cirlce = 3.142r2 =', area)
