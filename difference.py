35f43e4a0747e948381887459837b4b4d62d4460
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-06-21 18:44:56 +0500

    Difference can't be negative

---
+++ difference.py
@@ -0,0 +1,10 @@
+#Question num 7:
+
+'''   the difference between a given number and 17, difference cannot be negative   '''
+
+num = int(input('enter your num'))
+given_num = 17
+
+diff = (num - given_num)
+print('given num ' ,given_num)
+print('difference' , diff )
