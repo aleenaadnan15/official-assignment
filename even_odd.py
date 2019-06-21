7baa406b45c98c42350089f9d5ff42ab1e587534
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-06-21 22:21:41 +0500

    Even or odd

---
+++ even_odd.py
@@ -0,0 +1,9 @@
+#Question num 10:
+
+'''   Check the num is even or odd  '''
+
+num = int(input('enter your num'))
+if (num % 2) == 0:
+    print('given num' ' is even')
+else:
+    print('given num' ' is odd')
