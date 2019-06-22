db76064e946ba7ca5325f68bf26b37e2283796db
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-06-22 16:37:58 +0500

    Solve the equation

---
+++ equation.py
@@ -0,0 +1,10 @@
+#Question num 14:
+
+'''   Solve the equation   '''
+
+x = int(input('enter x value:'))
+y = int(input('enter y value:'))
+
+equ_solve = (x + y) * (x + y)
+print('equation = (x + y) * (x + y)')
+print('equ_solve:' ,equ_solve)
