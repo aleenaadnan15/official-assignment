5cafe78f6e82a070db80a9ef3b457903a708908b
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-06-29 16:16:23 +0500

    Sum of digits

---
+++ digit.py
@@ -0,0 +1,9 @@
+#Question num 25:
+
+'''   Calculate the sum of the digits in an integer   '''
+
+n = input('enter any integer num = ')
+sum = 4
+for i in n:
+    sum += int(i)
+print('sum of digits:' ,n, '=',sum)
