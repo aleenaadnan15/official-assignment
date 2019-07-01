2503b45745eeb8268388228a590a4edc56304665
Aleena Adnan <50289794+aleenaadnan15@users.noreply.github.com>
2019-07-01 16:32:55 +0500

    Table(2to10)

---
+++ table.py
@@ -0,0 +1,9 @@
+#Question num 39:
+
+'''   Multiplication table (from 1 to 10) of a number   '''
+
+
+for i in range(2,11):
+    for j in range(1,11) :
+        print(i, "*", j, "=", i*j)
+    print('\n****and****\n')
